e94763d1f56865ab061516b59a573c7d7e9ac653
Andbeav <38459485+Andbeav@users.noreply.github.com>
2022-01-11 20:56:36 +0200

    V1.2.0 (#3)
    * initial working keybind implementation
    
    * Double toggle sneak implemented
    
    * Apply backup defaults and bump version

--- src/main/java/net/fabricmc/safetweaks/SafeTweaksClient.java
+++ src/main/java/net/fabricmc/safetweaks/SafeTweaksClient.java
@@ -1,7 +1,17 @@
 package net.fabricmc.safetweaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.safetweaks.config.FeatureFlagManager;
 // import net.fabricmc.safetweaks.config.FeatureFlagManager;
+import net.fabricmc.safetweaks.config.KeyBindManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.TranslatableText;
+
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,20 +28,42 @@ public class SafeTweaksClient implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-        // Set feature flags on launch | Apparently mixin runs first...
-        // initializeFeatureFlags();
-
 		LOGGER.info("SafeTweaks init!");
+
+        registerKeyBinds();
+        registerDoubleSneakToggle();
 	}
 
-    // private void initializeFeatureFlags() {
-    //     // Will populate from the given file in FeatureFlagManager constructor
-    //     FeatureFlagManager.getInstance((instance) -> {
-    //         // Skip setting defaults unless the flags are empty
-    //         if(instance.isEmpty()) { return; }
+    private void registerKeyBinds() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            FeatureFlagManager flagManager = FeatureFlagManager.getInstance();
+            for (Map.Entry<String, KeyBinding> entry : getKeyBindEntrySet()) {
+                while(entry.getValue().wasPressed()) {
+                    flagManager.set(entry.getKey(), !flagManager.get(entry.getKey()));
+                    FeatureFlagManager.saveFlagsPersistent();
+                    client.player.sendMessage(new LiteralText("SafeTweaks: Toggled " + new TranslatableText(entry.getKey()).getString()), false);
+                }
+            }
 
-    //         // Set default flags here
-    //         instance.set("renderDistanceFogToggle", false);
-    //     });
-    // }
+            // while(KeyBinding.wasPressed()) {
+            //     client.player.sendMessage(new LiteralText("Unknown key was pressed"), false);
+            // }
+        });
+    }
+
+    private void registerDoubleSneakToggle() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            MinecraftClient mc = MinecraftClient.getInstance();
+            if(mc.player == null) { return; }
+            if(FeatureFlagManager.getInstance().get("config.safetweaks.double-sneak", false)) {
+                mc.options.sneakToggled = true;
+            } else {
+                mc.options.sneakToggled = false;
+            }
+        });
+    }
+
+    private Set<Map.Entry<String,KeyBinding>> getKeyBindEntrySet() {
+        return KeyBindManager.getInstance().getKeyBindings().entrySet();
+    }
 }
